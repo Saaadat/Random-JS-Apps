30a862f6b5cf2616bffb45bf9ca6110d582d5353
Sa'aadat Shafeeq Lone <saaadatlone@gmail.com>
2019-09-07 22:21:41 +0800

    finished express example

---
+++ Express Example/Members.js
@@ -0,0 +1,19 @@
+const members = [
+    {
+    id: 1,
+    name: 'Saaadat Lone',
+    email: 'saaadatlone@gmail.com',
+    status: 'active'},
+    {
+    id: 2,
+    name: 'Doe John',
+    email: 'test@gmail.com',
+    status: 'active'},
+    {
+    id: 3,
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    status: 'inactive'},
+]
+
+module.exports = members;
